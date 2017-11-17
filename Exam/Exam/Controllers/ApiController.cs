3b9f34a316584479b4f94479bca96def683c2f72
Csilla Varga <vargacsillamagda@gmail.com>
2017-11-17 11:22:04 +0100

    Exam - search by brand API endpoint added

--- Exam/Exam/Controllers/ApiController.cs
+++ Exam/Exam/Controllers/ApiController.cs
@@ -3,41 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Exam.Repositories;
 
 namespace Exam.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api")]
     public class ApiController : Controller
     {
+        ExamRepository ExamRepository;
+
+        public ApiController(ExamRepository examRepository)
+        {
+            ExamRepository = examRepository;
+        }
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        [Route("search/{brand}")]
+        public IActionResult SearchByBrand(string brand)
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var searchResult = new
+            {
+                result = "ok",
+                data = ExamRepository.SearchByBrand(brand)
+            };
+            return Json(searchResult);
         }
     }
 }
